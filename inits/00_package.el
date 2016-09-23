e1c4779d492863e47682877de953c310b420fdd4
Rumisbern <tail.zabiatsuta@gmail.com>
2016-09-24 04:08:29 +0900

    MELPAのURL追加

---
+++ inits/00_package.el
@@ -0,0 +1,7 @@
+;; MELPA パッケージ管理 を利用
+(require 'package)
+
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
