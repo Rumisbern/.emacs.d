e9b7f1454f5feec462a4a1812b9e9eb5659dbe23
Rumisbern <tail.zabiatsuta@gmail.com>
2016-09-24 15:39:00 +0900

    markdown-modeでプレビューを外部コマンドで呼び出す記述追加

--- inits/60_markdown-mode.el
+++ inits/60_markdown-mode.el
@@ -9,3 +9,14 @@
 	 ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown"))
 
+;; multimarkdownコマンドがあれば
+;; C-c C-c m で別バッファにhtmlファイルを出力
+;; C-c C-c p で一時ファイルを生成し、ブラウザを起動
+(when (executable-find "multimarkdown")
+  (setq markdown-command "multimarkdown"))
+
+;; Marked 2 (OS X)の起動スクリプト marked2 パス内があれば
+;; C-c C-c o でMarked 2が起動する
+(when (executable-find "marked2")
+  (setq markdown-open-command "marked2"))
+
