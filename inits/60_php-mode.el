09da510bd644bd2a5c9c3736f45386ab050277a0
Rumisbern <tail.zabiatsuta@gmail.com>
2016-10-10 20:41:26 +0900

    php-mode update

--- inits/60_php-mode.el
+++ inits/60_php-mode.el
@@ -4,7 +4,10 @@
 ;;; (package-install 'php-mode)
 (use-package php-mode
   :config
-  (setq web-mode-markup-indent-offset 2))
+  (setq web-mode-markup-indent-offset 2)
+  (setq tab-width 2)
+  (setq c-basic-offset 2)
+  (setq indent-tabs-mode nil))
 
 (use-package web-mode
   :init
