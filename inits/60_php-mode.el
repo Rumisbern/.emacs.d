204ad1914cc9f76a039f12e80d2fd0e3ab1f1e0b
Rumisbern <tail.zabiatsuta@gmail.com>
2016-10-24 06:34:10 +0900

    php-modeにweb-modeをhookしないように変更＆complete-php等追加

--- inits/60_php-mode.el
+++ inits/60_php-mode.el
@@ -2,14 +2,25 @@
 
 ;;; (package-install 'web-mode)
 ;;; (package-install 'php-mode)
+;;; (package-install 'company-php)
+;;; (package-install 'company-web)
+
 (use-package php-mode
+  :mode (("\\.php\\'"   . php-mode))
   :config
   (setq web-mode-markup-indent-offset 2)
   (setq tab-width 2)
   (setq c-basic-offset 2)
   (setq indent-tabs-mode nil))
 
-(use-package web-mode
-  :init
-  (add-hook 'php-mode-hook 'web-mode))
+;; (use-package web-mode
+;;   :mode (("\\.html?\\'" . web-mode)
+;;          ("\\.php\\'"   . web-mode)))
 
+(use-package company-php
+  :init
+  (add-hook 'php-mode-hook 'company-php))
+
+(use-package company-web
+  :init
+  (add-hook 'php-mode-hook 'company-web))
