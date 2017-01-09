1ff883f9e47f7b140e74dad57648c5cd99716444
Rumisbern <tail.zabiatsuta@gmail.com>
2017-01-10 01:41:55 +0900

    neotreeのキーバインドを'C-c t'に変更

--- inits/51_neotree.el
+++ inits/51_neotree.el
@@ -2,7 +2,7 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/neotree")
 (require 'neotree)
 ;; F8キーで開く
-(global-set-key [f8] 'neotree-toggle)
+(global-set-key (kbd "C-c t") 'neotree-toggle)
 ;; 隠しファイルをデフォルトで表示
 (setq neo-show-hidden-files t)
 
