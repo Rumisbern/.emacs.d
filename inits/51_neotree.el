66325890e9c2125cc99db57798a397fe3553c26e
Rumisbern <tail.zabiatsuta@gmail.com>
2017-01-09 18:39:50 +0900

    neotreeの隠しファイルをデフォルトで見えるように変更

--- inits/51_neotree.el
+++ inits/51_neotree.el
@@ -1,6 +1,8 @@
 ;;;; ディレクトリツリーneotree
-;; F8キーで開く
 (add-to-list 'load-path "~/.emacs.d/site-lisp/neotree")
 (require 'neotree)
+;; F8キーで開く
 (global-set-key [f8] 'neotree-toggle)
+;; 隠しファイルをデフォルトで表示
+(setq neo-show-hidden-files t)
 
