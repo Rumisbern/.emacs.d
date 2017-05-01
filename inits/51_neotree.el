b265f8f7720ff3e15bf39694feb92a8316a438d8
kotani <ryunosuke.kotani@userlocal.jp>
2017-05-01 13:07:51 +0900

    neotreeの検索機能追加

--- inits/51_neotree.el
+++ inits/51_neotree.el
@@ -5,4 +5,16 @@
 (global-set-key (kbd "C-c t") 'neotree-toggle)
 ;; 隠しファイルをデフォルトで表示
 (setq neo-show-hidden-files t)
+;; ファイル検索
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (ffip-project-root))
+	(file-name (buffer-file-name)))
+    (if project-dir
+	(progn
+	  (neotree-dir project-dir)
+	  (neotree-find file-name))
+      (message "Could not find git project root."))))
+(define-key neotree-mode-map (kbd "C-c C-p") 'neotree-project-dir)
 
