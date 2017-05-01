a5174f87c918449d4dfc7083f704944d0196d7b7
kotani <ryunosuke.kotani@userlocal.jp>
2017-05-01 13:09:12 +0900

    C-hで削除

--- inits/02_global-settings.el
+++ inits/02_global-settings.el
@@ -1,3 +1,6 @@
+;; tab -> space
+(setq-default indent-tabs-mode nil)
+
 ;; whitespace
 (require 'whitespace)
 (setq whitespace-style '(face
@@ -9,3 +12,9 @@
 (setq whitespace-display-mappings
       '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
 (global-whitespace-mode 1)
+
+;; delete-backward-char by C-h
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+
+;; help prefix by C-?
+(global-set-key (kbd "M-?") 'help-for-help)
