ba3c4120630746e7687c754ebab1770503d818e3
Ryunosuke Kotani <ryunosuke.kotani@userlocal.jp>
2017-07-20 18:02:34 +0900

    cocoa emacs用の設定追加

--- inits/02_global-settings.el
+++ inits/02_global-settings.el
@@ -4,11 +4,11 @@
 ;; whitespace
 (require 'whitespace)
 (setq whitespace-style '(face
-			 trailing
-			 tabs
-			 space-mark
-			 tab-mark
-			 ))
+                         trailing
+                         tabs
+                         space-mark
+                         tab-mark
+                         ))
 (setq whitespace-display-mappings
       '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
 (global-whitespace-mode 1)
@@ -18,3 +18,11 @@
 
 ;; help prefix by C-?
 (global-set-key (kbd "M-?") 'help-for-help)
+
+;; for window system
+(if window-system
+    (progn
+      ;; フルスクリーン
+      (set-frame-parameter nil 'fullscreen 'fullboth)
+      (set-face-attribute 'default nil :height 150)
+      (scroll-bar-mode 0)))
