f64170b896ce670b3ee2b9b5421a8d1b7b9ff522
Rumisbern <tail.zabiatsuta@gmail.com>
2018-11-14 11:50:47 +0900

    add go-mode

---
+++ inits/60_go-mode.el
@@ -0,0 +1,20 @@
+;;;; go-mode
+
+(add-to-list 'exec-path (expand-file-name (concat (getenv "HOME") "/go/bin")))
+(add-to-list 'exec-path (expand-file-name (concat (getenv "HOME") "/.go/bin")))
+
+(use-package go-mode
+  :init
+  (add-hook 'go-mode-hook 'company-mode)
+  (add-hook 'go-mode-hook 'flycheck-mode)
+  (add-hook 'go-mode-hook (lambda()
+           (add-hook 'before-save-hook' 'gofmt-before-save)
+           (local-set-key (kbd "M-.") 'godef-jump)
+           (set (make-local-variable 'company-backends) '(company-go))
+           (company-mode)
+           (setq indent-tabs-mode nil)
+           (setq c-basic-offset 2)
+           (setq tab-width 2)))
+  )
+(use-package company-go)
+
